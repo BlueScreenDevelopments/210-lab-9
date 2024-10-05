ab42938810a56691da2bd466e4b402502e04a5e8
Brad Julao <pogiboybrad@gmail.com>
2024-10-04 20:10:43 -0700

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    // complete this code in order to make it output
-    // the numbers 1-100
+    int i;
 
-    for ()
+    for (i = 1; i <= 100; i++) {
         cout << i << " ";
+    }
 
     return 0;
 }
